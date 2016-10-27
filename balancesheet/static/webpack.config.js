355611ed97e505d7dd708ce2b0816a22321deb7e
Pommes Klaus <christianfritz.mail@gmail.com>
2016-10-27 09:22:40 +0000

    Added vuejs-app to django-app

--- balancesheet/static/webpack.config.js
+++ balancesheet/static/webpack.config.js
@@ -4,7 +4,7 @@ var webpack = require('webpack')
 module.exports = {
   entry: [
     'webpack-dev-server/client?https://0.0.0.0:8080', // WebpackDevServer host and port
-    'webpack/hot/only-dev-server', // "only" prevents reload on syntax errors
+    //'webpack/hot/only-dev-server', // "only" prevents reload on syntax errors
     './src/main.js' // Your appʼs entry point
   ],
   
