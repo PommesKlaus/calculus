cefc5d4d39ed21f11fb9e368fe2b3395c88271e5
Pommes Klaus <christianfritz.mail@gmail.com>
2016-10-28 08:58:35 +0000

    Added form-tag to Details page

--- balancesheet/static/src/components/details.vue
+++ balancesheet/static/src/components/details.vue
@@ -1,67 +1,80 @@
 <template>
   <div class="container">
-    <div class="col-md-2">Bilanzlinie:</div>
-    <div class="col-md-10">Bla Bla</div>
-
-    <div class="col-md-2">Sachverhalt:</div>
-    <div class="col-md-10">{{ difference.name }}</div>
-
-    <table class="table table-hover">
-      <thead>
-        <tr>
-          <th class="col-md-2"></th>
-          <th class="col-md-2">Vorperiode</th>
-          <th class="col-md-2"></th>
-          <th class="col-md-2">Matching Vorperiode</th>
-          <th class="col-md-2"></th>
-          <th class="col-md-2">lfd. Periode</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>Local GAAP</td>
-          <td>{{ difference.py_local_gaap }}</td>
-          <td></td>
-          <td>{{ difference.tu_local_gaap }}</td>
-          <td></td>
-          <td>{{ difference.local_gaap }}</td>
-        </tr>
-        <tr>
-          <td>Tax GAAP</td>
-          <td>{{ difference.py_tax_gaap }}</td>
-          <td></td>
-          <td>{{ difference.tu_tax_gaap }}</td>
-          <td></td>
-          <td>{{ difference.tax_gaap }}</td>
-        </tr>
-        <tr>
-          <td>Differenz</td>
-          <td>{{ difference.py_difference }}</td>
-          <td></td>
-          <td>{{ difference.tu_difference }}</td>
-          <td></td>
-          <td>{{ difference.difference }}</td>
-        </tr>
-      </tbody>
-    </table>
+    <form v-on:submit.prevent="onSubmit">
+  
+      <div class="col-md-2">Bilanzlinie:</div>
+      <div class="col-md-10">Bla Bla</div>
+  
+      <div class="col-md-2">Sachverhalt:</div>
+      <div class="col-md-10"></div>
+  
+      <table class="table table-hover">
+        <thead>
+          <tr>
+            <th class="col-md-2"></th>
+            <th class="col-md-2">Vorperiode</th>
+            <th class="col-md-2"></th>
+            <th class="col-md-2">Matching Vorperiode</th>
+            <th class="col-md-2"></th>
+            <th class="col-md-2">lfd. Periode</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Local GAAP</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td><input v-model.lazy="formData.local_gaap" class="form-control input-sm"></td>
+          </tr>
+          <tr>
+            <td>Tax GAAP</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td><input v-model="formData.tax_gaap" class="form-control input-sm"></td>
+          </tr>
+          <tr>
+            <td>Differenz</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+        </tbody>
+      </table>
+    </form>
   </div>
 </template>
 
 <script>
 import { mapState } from 'vuex'
-module.exports = {  
+module.exports = {
   data: function() {
     return {
-      }
+      formData: {}
+    }
   },
-  computed: mapState({
-    Differences: 'Differences',
-    difference (state) {
-      let r = this.$route.params.differenceId
-      return state.Differences.find(function(x) { return x.id == r })
+  methods: {
+    onSubmit () {
+      return
+    }
+  },
+  computed: {
+    Difference () {
+        let r = this.$route.params.differenceId
+        return this.$store.state.Differences.find(function(x) { return x.id == r })
     },
-    LineItems: 'LineItems'
-  })
+    LineItems() { return this.$store.state.LineItems }
+  },
+  created() {
+    // Copy difference-data from state to be used as form model.
+    // Changes are only merged to state/store if form is submitted.
+    this.formData = Object.assign({}, this.Difference)
+  }
 }
 </script>
 
@@ -69,7 +82,12 @@ module.exports = {
 th {
   text-align: center;
 }
+
 td:nth-child(n+2) {
+  text-align: right;
+}
+
+td:nth-child(n+2) input {
   text-align: right;
 }
 </style>
